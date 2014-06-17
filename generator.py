7dc4eaf3555453d51e751edec5a8d5a501239390
Stas Parshin <pengrad@bk.ru>
2014-06-17 17:21:55 +0400

    Refactoring. Clean

--- generator.py
+++ generator.py
@@ -1,3 +1,4 @@
+import ast
 from genericpath import isfile, exists
 from os import listdir, makedirs
 from os.path import join
@@ -27,18 +28,11 @@ def process_dir(directory, mapping):
         for m in mapping:
             file = report_name(m["report_name"], tpl_file)
             res = file2str(tpl_file, directory).format_map(m)
-            # print(file2str(tpl_file, directory).format_map(m), "\n\n\n")
             str2file(res, file, join(directory, REPORT_DIR))
-            # open(join)
 
 
-# print("select {col} from {table}".format(col="id", table="login"))
-# print("select {col} from {table}".format_map({"col": 1, "table": "login", "name": 123, "row": "1adsa"}))
-# print(tpl2str("reports_apps/device_dau.tpl").format_map({"game_id":21}))
+s = '({"report_name": "ZR iOS App (3)", "game_id": 3},{"report_name": "ZR Android App (4)", "game_id": 4})'
+e = ast.literal_eval(s)
 
-process_dir("reports_apps", ({"report_name": "Snark Busters: Welcome to the Club (Alawar) (41)", "game_id": 21},
-                             {"report_name": "ZR iOS App (3)", "game_id": 777}))
-
-
-# str2file("text", "1.txt", "reports_apps/sql")
+process_dir("reports_apps", e)
 
